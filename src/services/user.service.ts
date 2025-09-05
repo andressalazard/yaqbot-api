6fc06f600477f2a5d681c89a38bab434dcf490b3
Andrés Salazar <andres.salazar.d.18@gmail.com>
2025-09-05 16:26:03 -0500

    implements prisma logic

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,37 +1,94 @@
 import dbpool from '../database/postgresql.database';
+import { AppError } from '../errors/AppError';
+import { prisma } from '../lib/prisma';
 import { UpdatedUserData } from '../types';
 
 export class UserService {
   //GET
   static async getAllUsers() {
-    const result = await dbpool.query(`SELECT * FROM "YQBOT_VIEW"."V_Users"`);
-    return result.rows;
+    return await prisma.user.findMany({
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        role: true,
+        createdAt: true,
+      },
+    });
+  }
+
+  static async getPublicUsers() {
+    return await prisma.user.findMany({
+      select: {
+        name: true,
+      },
+    });
   }
 
   static async getUserById(id: string) {
-    const result = await dbpool.query(`SELECT * FROM "YQBOT_DATA"."Users" WHERE id = $1`, [id]);
-    return result.rows[0] || null;
+    const user = await prisma.user.findUnique({
+      where: { id },
+      include: {
+        profile: {
+          select: {
+            avatar: true,
+            firstname: true,
+            lastname: true,
+          },
+        },
+      },
+    });
+
+    if (!user) {
+      throw new AppError('Usuario no encontrado', 404);
+    }
+    return user;
+  }
+
+  static async getPublicUserById(id: string) {
+    const user = await prisma.user.findUnique({
+      where: { id },
+      select: {
+        username: true,
+        email: true,
+      },
+    });
+
+    if (!user) {
+      throw new AppError('Usuario no encontrado', 404);
+    }
+
+    return user;
   }
 
   //UPDATE
   static async updateUser(id: string, data: UpdatedUserData) {
-    const keys = Object.keys(data);
-    const values = Object.values(data);
+    const existingUser = await prisma.user.findUnique({ where: { id } });
 
-    if (keys.length === 0) {
-      throw new Error('No hay datos para actualizar');
+    if (!existingUser) {
+      throw new AppError('Usuario no encontrado', 404);
     }
 
-    const setValues = keys.map((key, index) => `${key} = $${index + 1}`).join(', ');
-
-    await dbpool.query(`UPDATE "YQBOT_DATA"."Users" SET ${setValues} WHERE id = $${keys.length + 1};`, [...values, id]);
-    const result = await dbpool.query(`SELECT * FROM "YQBOT_DATA"."Users" WHERE id = $1`, [id]);
-    return result.rows[0] || null;
+    return await prisma.user.update({
+      where: { id },
+      data, //only updates provided fields
+      select: {
+        id: true,
+        username: true,
+        email: true,
+        role: true,
+      },
+    });
   }
 
   //DELETE
   static async deleteUser(id: string) {
-    const result = await dbpool.query(`DELETE FROM "YQBOT_DATA"."Users" WHERE id = $1`, [id]);
-    return result.rows[0] || null;
+    const existingUser = await prisma.user.findUnique({ where: { id } });
+
+    if (!existingUser) {
+      throw new AppError('Usuario no encontrado', 404);
+    }
+    await prisma.user.delete({ where: { id } });
+    return { message: 'Usuario eliminado con éxito' };
   }
 }
